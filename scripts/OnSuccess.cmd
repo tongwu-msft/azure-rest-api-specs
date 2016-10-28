9bd2c7240b8d1d4bf6e66b76a7f2a26177526027
jehuan <jehuan@microsoft.com>
2016-10-28 17:46:25 +0800

    refine CI messages

--- scripts/OnSuccess.cmd
+++ scripts/OnSuccess.cmd
@@ -1,3 +1,2 @@
 git add . 
-git diff --quiet --exit-code --cached || git commit -m "Rest document Updated by CI" && git push -u origin master
-echo "Document updated"
+git diff --quiet --exit-code --cached || git commit -m "Rest document Updated by CI" && git push -u origin master && echo "Document updated"
